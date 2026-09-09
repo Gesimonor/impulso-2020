f0a0e5eb10f4c1d61d4d1bcb12d0b1033ae0ff9a
Gesimonor <luzestela4568@gmail.com>
2026-09-08 21:45:53 -0500

    Act

--- logica_formulas.py
+++ logica_formulas.py
@@ -69,9 +69,9 @@ def crear_formula(paciente_id, prox_control, fecha,fecha_vencimiento=None, obser
     db.refresh(nueva_formula)
     return nueva_formula
 
-def consultar_formulas_por_paciente(paciente_id):
+def consultar_formula_para_editar(id):
     with SessionLocal() as db:
-        formulas = db.query(Formula).filter(Formula.paciente_id == paciente_id).all()
+        formulas = db.query(Formula).filter(Formula.id == id).first()
     return formulas
 
 def eliminar_formula(formula_id):
@@ -83,17 +83,33 @@ def eliminar_formula(formula_id):
             return True
         return False
 
-def editar_formula(formula_id, **kwargs):
+def editar_formula(id, paciente_id, prox_control, fecha, fecha_vencimiento, observaciones,
+                   od_esfera, od_cilindro, od_eje, od_adicion, od_alturabifocal, od_distanciainterpupilar, od_color,
+                   oi_esfera, oi_cilindro, oi_eje, oi_adicion, oi_alturabifocal, oi_distanciainterpupilar, oi_color):
     with SessionLocal() as db:
-        formula = db.get(Formula, formula_id)
-        if formula:
-            for key, value in kwargs.items():
-                if hasattr(formula, key):
-                    setattr(formula, key, value)
-            db.commit()
-            db.refresh(formula)
-            return formula
-        return None
+        formula_db = db.query(Formula).filter(Formula.id == id).first()
+        formula_db.paciente_id = paciente_id
+        formula_db.prox_control = prox_control
+        formula_db.fecha = fecha
+        formula_db.fecha_vencimiento = fecha_vencimiento
+        formula_db.observaciones = observaciones
+        formula_db.od_esfera = od_esfera
+        formula_db.od_cilindro = od_cilindro
+        formula_db.od_eje = od_eje
+        formula_db.od_adicion = od_adicion
+        formula_db.od_alturabifocal = od_alturabifocal
+        formula_db.od_distanciainterpupilar = od_distanciainterpupilar
+        formula_db.od_color = od_color
+        formula_db.oi_esfera = oi_esfera
+        formula_db.oi_cilindro = oi_cilindro
+        formula_db.oi_eje = oi_eje
+        formula_db.oi_adicion = oi_adicion
+        formula_db.oi_alturabifocal = oi_alturabifocal
+        formula_db.oi_distanciainterpupilar = oi_distanciainterpupilar
+        formula_db.oi_color = oi_color
+        db.commit()
+        db.refresh(formula_db)
+        return formula_db
 
 def listar_formulas():
     db = SessionLocal()
--- rutas/bp_formulas.py
+++ rutas/bp_formulas.py
@@ -1,6 +1,6 @@
 from flask import Blueprint, render_template, request, redirect, url_for, flash
 from flask_login import login_required, current_user
-from logica_formulas import crear_formula, consultar_formulas_por_paciente, editar_formula, eliminar_formula, listar_formulas
+from logica_formulas import crear_formula, consultar_formula_para_editar, editar_formula, eliminar_formula, listar_formulas
 from  datetime import datetime
 from dateutil.relativedelta import relativedelta
 
@@ -8,7 +8,7 @@ blueprint_formulas = Blueprint("bluep_formulas", __name__) #le decimos que es un
 
 @blueprint_formulas.route("/formulas", methods=["GET", "POST"]) #Get es para traer la pagina y POST es para enviar los datos del formulario
 @login_required
-def crear_formula_route():
+def crear_formula_route(): #Crea y precarga la lista de las formulas 
     if request.method == "POST":
         prox_control_int= int(request.form.get("prox_control"))
         prox_control_date = (datetime.strptime(request.form.get("fecha"), "%Y-%m-%d")) + relativedelta(months=prox_control_int)
@@ -37,35 +37,11 @@ def crear_formula_route():
     formulas = listar_formulas()
     return render_template("formulas.html", usuario=current_user, formulas=formulas)
 
-@blueprint_formulas.route("/formulas/editar/<int:id>", methods=["GET", "POST"])
+@blueprint_formulas.route("/formulas/editar/<int:id>", methods=["GET", "POST"]) 
 @login_required
-def editar_formula_route(id):
-    formula = consultar_formulas_por_paciente(id)
-    if request.method == "POST":
-        editar_formula(
-            id=id,
-            paciente_id=request.form.get("paciente_id"),
-            prox_control=request.form.get("prox_control"),
-            fecha=datetime.strptime(request.form.get("fecha"), "%Y-%m-%d"),
-            fecha_vencimiento=datetime.strptime(request.form.get("fecha_vencimiento"), "%Y-%m-%d") if request.form.get("fecha_vencimiento") else None,
-            observaciones=request.form.get("observaciones"),
-            od_esfera=request.form.get("od_esfera"),
-            od_cilindro=request.form.get("od_cilindro"),
-            od_eje=request.form.get("od_eje"),
-            od_adicion=request.form.get("od_adicion"),
-            od_alturabifocal=request.form.get("od_alturabifocal"),
-            od_distanciainterpupilar=request.form.get("od_distanciainterpupilar"),
-            od_color=request.form.get("od_color"),
-            oi_esfera=request.form.get("oi_esfera"),
-            oi_cilindro=request.form.get("oi_cilindro"),
-            oi_eje=request.form.get("oi_eje"),
-            oi_adicion=request.form.get("oi_adicion"),
-            oi_alturabifocal=request.form.get("oi_alturabifocal"),
-            oi_distanciainterpupilar=request.form.get("oi_distanciainterpupilar"),
-            oi_color=request.form.get("oi_color")
-        )
-        flash("Fórmula editada exitosamente")
-    return redirect(url_for("bluep_formulas.crear_formula_route"))
+def editar_formula_route(id): #EDita y prellena los campos 
+    formula = consultar_formula_para_editar(id)
+    return render_template("editar_formula.html",  usuario=current_user,formula=formula) #GET y se usa  usuario=current_user porque en la base.html tenemos un Bienvenido <usuario ese el nombre y debe llenar ese hueco con susuario
 
 @blueprint_formulas.route("/formulas/eliminar/<int:id>", methods=["POST"])
 @login_required

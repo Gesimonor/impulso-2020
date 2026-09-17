2637ae133a805abd5ffa60cf89f1f6b5a271cd97
Gesimonor <luzestela4568@gmail.com>
2026-09-16 21:47:06 -0500

    Act

--- rutas/bp_pacientes.py
+++ rutas/bp_pacientes.py
@@ -7,50 +7,62 @@ from  datetime import datetime
 blueprint_pacientes = Blueprint("bluep_pacientes", __name__) #le decimos que es un blueprint y que se llama pacientes, el __name__ es para decirle a Flask que este archivo es el que tiene las rutas de pacientes
 """Request es un objeto de Flask que contiene toda la información de la petición que llegó.
     request.method      → "GET" o "POST"
-    request.form        → los datos del formulario
+    request.form        → los datos del formulario y esto es POST y e mas seguro
     request.form.get("nombre")  → el valor del campo "nombre"
     request.url         → la URL completa
-    request.args        → parámetros de la URL (?buscar=juan)
+    request.args        → parámetros de la URL (?buscar=juan) esto es GET
  """
-@blueprint_pacientes.route("/pacientes",methods=["GET", "POST"]) #Get es para traer la pagina y POST es para enviar los datos del formulario
+
+@blueprint_pacientes.route("/pacientes",methods=["GET","POST"]) 
+@login_required
+def listar_pacientes_route ():
+    valor_buscar = request.args.get("valor_buscar")
+    if valor_buscar is None or valor_buscar == "":
+        pacientes = listar_pacientes()
+        return render_template("pacientes.html", usuario=current_user, pacientes=pacientes)
+    else:
+        lista_consultada = consultar_pordatos(valor_buscar)
+        return render_template("pacientes.html", usuario=current_user, pacientes=lista_consultada)
+
+@blueprint_pacientes.route("/pacientes/crear-paciente",methods=["POST"]) #Get es para traer la pagina y POST es para enviar los datos del formulario
 @login_required
 #request es un objeto de Flask que contiene toda la información de la petición que llegó
 def crear_paciente_route():
+    crear_paciente(
+        documento=request.form.get("documento"),
+        nombre=request.form.get("nombre"),
+        apellido=request.form.get("apellido"),
+        celular=request.form.get("celular"),
+        correo=request.form.get("correo"),
+        fecha_nacimiento=datetime.strptime(request.form.get("fecha_nacimiento"), "%Y-%m-%d"), #Esto convierte el texto en una fecha legible para la logicas
+        direccion=request.form.get("direccion")
+        )
+    flash("Paciente creado exitosamente") #flash es una funcion de Flask que sirve para mostrar mensajes en la pagina web, en este caso se muestra un mensaje de exito cuando se crea un paciente    
+    return redirect(url_for("bluep_pacientes.listar_pacientes_route")) #1 va en blueprint_pacientes porque es el nombre del blueprint y 2 va en pacientes porque es el nombre de la funcion que tenemos arriba
+"""Sin redirect:
+    Usuario llena formulario → POST → paciente creado
+    Usuario presiona F5 (recargar)
+    → el navegador pregunta "¿reenviar el formulario?"
+    → ¡crea el paciente dos veces! 💀
+
+    Con redirect:
+    Usuario llena formulario → POST → paciente creado
+    → redirect a /pacientes → GET limpio
+    Usuario presiona F5
+    → solo recarga la página, no reenvía el formulario 
+"""
+
+@blueprint_pacientes.route("/pacientes/validar-documento",methods=["POST"]) #Get es para traer la pagina y POST es para enviar los datos del formulario
+@login_required
+#request es un objeto de Flask que contiene toda la información de la petición que llegó
+def validar_duplicado_route():
     if request.method == "POST":
         if validar_duplicado(request.form.get("documento")) == True:
             flash("El documento ya está registrado")
             #return render_template("app-layout.html", usuario="Prueba")
         else:
-            crear_paciente(
-                documento=request.form.get("documento"),
-                nombre=request.form.get("nombre"),
-                apellido=request.form.get("apellido"),
-                celular=request.form.get("celular"),
-                correo=request.form.get("correo"),
-                fecha_nacimiento=datetime.strptime(request.form.get("fecha_nacimiento"), "%Y-%m-%d"), #Esto convierte el texto en una fecha legible para la logicas
-                direccion=request.form.get("direccion")
-                )
-            flash("Paciente creado exitosamente") #flash es una funcion de Flask que sirve para mostrar mensajes en la pagina web, en este caso se muestra un mensaje de exito cuando se crea un paciente    
-            return redirect(url_for("bluep_pacientes.crear_paciente_route")) #1 va en blueprint_pacientes porque es el nombre del blueprint y 2 va en pacientes porque es el nombre de la funcion que tenemos arriba
-        """Sin redirect:
-            Usuario llena formulario → POST → paciente creado
-            Usuario presiona F5 (recargar)
-            → el navegador pregunta "¿reenviar el formulario?"
-            → ¡crea el paciente dos veces! 💀
-
-            Con redirect:
-            Usuario llena formulario → POST → paciente creado
-            → redirect a /pacientes → GET limpio
-            Usuario presiona F5
-            → solo recarga la página, no reenvía el formulario 
-        """
-    if request.args.get("valor_buscar") == None:
-        pacientes = listar_pacientes()
-        return render_template("pacientes.html", usuario=current_user, pacientes=pacientes)
-    else:
-        lista_consultada = consultar_pordatos(request.args.get("valor_buscar"))
-        return render_template("pacientes.html", usuario=current_user, pacientes=lista_consultada)
-
+            flash("El documento no está registrado")
+            return redirect(url_for("bluep_pacientes.listar_pacientes_route")) #1 va en blueprint_pacientes porque es el nombre del blueprint y 2 va en pacientes porque es el nombre de la funcion que tenemos arriba
 
 @blueprint_pacientes.route("/pacientes/editar/<int:id>", methods=["GET", "POST"])
 @login_required 
@@ -68,7 +80,7 @@ def editar_paciente_route(id):
             direccion=request.form.get("direccion")
         )
         flash("Paciente editado exitosamente")
-        #return redirect(url_for("bluep_pacientes.pacientes"))
+        return redirect(url_for("bluep_pacientes.listar_pacientes_route"))
     return render_template("editar_paciente.html", usuario=current_user, paciente=paciente)
 
 @blueprint_pacientes.route("/pacientes/eliminar/<int:id>", methods=["POST"])
@@ -76,7 +88,7 @@ def editar_paciente_route(id):
 def eliminar_paciente_route(id):
     eliminar_paciente(id)
     flash("Paciente eliminado exitosamente")
-    return redirect(url_for("bluep_pacientes.crear_paciente_route"))
+    return redirect(url_for("bluep_pacientes.listar_pacientes_route"))
 
 
 @blueprint_pacientes.route("/pacientes/ver/<int:id>", methods=["GET"])
